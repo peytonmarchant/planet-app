ee09a7e6f7594ff097c0c0851e5e5066b0297c8c
Peyton Marchant <peyton@farmcomics.com>
2016-11-13 09:28:49 -0500

    adding data load

--- web/js/planets.js
+++ web/js/planets.js
@@ -5,32 +5,32 @@
      this.use('Template');
    }
 
-   this.get('#mercury', function() {
-     updatePlanet('Mercury');
+   this.get('#mercury', function(context) {
+     updatePlanet('Mercury', context);
    });
-   this.get('#venus', function() {
-     updatePlanet('Venus');
+   this.get('#venus', function(context) {
+     updatePlanet('Venus', context);
    });
-   this.get('#earth', function() {
-     updatePlanet('Earth');
+   this.get('#earth', function(context) {
+     updatePlanet('Earth', context);
    });
-   this.get('#mars', function() {
-     updatePlanet('Mars');
+   this.get('#mars', function(context) {
+     updatePlanet('Mars', context);
    });
-   this.get('#jupiter', function() {
-     updatePlanet('Jupiter');
+   this.get('#jupiter', function(context) {
+     updatePlanet('Jupiter', context);
    });
-   this.get('#saturn', function() {
-     updatePlanet('Saturn');
+   this.get('#saturn', function(context) {
+     updatePlanet('Saturn', context);
    });
-   this.get('#uranus', function() {
-     updatePlanet('Uranus');
+   this.get('#uranus', function(context) {
+     updatePlanet('Uranus', context);
    });
-   this.get('#neptune', function() {
-     updatePlanet('Neptune');
+   this.get('#neptune', function(context) {
+     updatePlanet('Neptune', context);
    });
-   this.get('#pluto', function() {
-     updatePlanet('Pluto');
+   this.get('#pluto', function(context) {
+     updatePlanet('Pluto', context);
    });
 
 
